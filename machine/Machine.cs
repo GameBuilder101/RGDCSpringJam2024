d532939c348bf616821c22d228e77864f220aae8
GiovanniDPaulino <gdp1388@g.rit.edu>
2024-02-24 13:21:00 -0500

    A

--- machine/Machine.cs
+++ machine/Machine.cs
@@ -26,16 +26,27 @@ public partial class Machine : Node2D
 	[Export]
 	public float JackpotProbability { get; private set; }
 	/// <summary>
+	/// The default chance for a guest to win the jackpot.
+	/// </summary>
+	[Export]
+	public float DefaultJackpotProbability { get; private set; }
+	
+	/// <summary>
 	/// The amount of times that a machine gets rolled in a tick.
 	/// </summary>
 	[Export]
 	public int NumRolls { get; private set; }
 
 	/// <summary>
-	/// The amount that suspicion increases each tick that a win has not occured.
+	/// The amount that suspicion increases each tick that a win has not occured. The formula for this is defined in Ready()
 	/// </summary>
 	[Export]
-	public float SuspicionFactor { get; private set; }
+	public float SuspicionFactor;
+	/// <summary>
+	/// The default amount that suspicion increases each tick that a win has not occured.
+	/// </summary>
+	[Export]
+	public float DefaultSuspicionFactor { get; private set; }
 
 	/*
 	[Export]
@@ -45,11 +56,14 @@ public partial class Machine : Node2D
 	public override void _Ready()
 	{
 		base._Ready();
+		SuspicionUpdater();
+		GD.Print(SuspicionFactor);
 		Tick();
 	}
 
 	public void Tick()
 	{
+		SuspicionUpdater();
 		int Revenue = 0;
 		for(int index = 0; index < NumRolls; index++){
 			Revenue += PlayCost;
@@ -82,6 +96,14 @@ public partial class Machine : Node2D
 		GD.Seed(seed);
 		float value = RNG.Randf();
 		return value;
+	}
+	
+	public void SuspicionUpdater()
+	{
+		SuspicionFactor = ((DefaultJackpotProbability/JackpotProbability)*DefaultSuspicionFactor)-DefaultSuspicionFactor;
+		if(SuspicionFactor < 0){
+			SuspicionFactor = 0;
+		}
 	}
 
 	/// <summary>
